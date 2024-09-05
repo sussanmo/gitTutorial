c7321d09f42a6c44c3b55cc0a76297f749ec31a1
Suad Mohamed <suad.mohamedh@gmail.com>
2024-09-05 10:45:21 -0500

    Initial commit: Added hello.txt, test.txt, logs.log

---
+++ main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("The average is: " +
+                average(numbers));
+    }
+    // Non-working average function (needs a fix)
+    public static double average(int[] nums) {
+        int sum = 0
+// Placeholder: This function is incomplete and currently returns 0
+        for int = 0: i < nums.length-1; i++{
+            //add every number in the array
+            sum = nums[i]+nums[i+1]
+        }
+        average = sum/ nums.length-1
+        return 0;
+    }
+}
